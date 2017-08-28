9119d36c7af7fa1628630f028d1ae96409ca613e
klemenStanic <stanic.klemen@gmail.com>
2017-08-28 11:09:56 +0200

    Time since last change correction.

--- src/IDSJava.java
+++ src/IDSJava.java
@@ -1,32 +1,15 @@
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.sun.org.apache.xpath.internal.SourceTree;
-import com.sun.xml.internal.bind.v2.TODO;
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkConf.*;
-import org.apache.spark.SparkContext;
-import org.apache.spark.SparkContext.*;
-import org.apache.spark.SparkContext.*;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.elasticsearch.spark.*;
-import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
-
-import scala.Tuple2;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.SparkConf;
-
 import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.*;
 
 
@@ -42,7 +25,7 @@ public class IDSJava {
         JavaSparkContext jsc = new JavaSparkContext(conf);
 
 
-        //perform ICMP tests, if there are more than THRESHOLD ICMP events, write a warning back to elasticsearch
+        //perform ICMP tests, if there are more than THRESHOLD ICMP events, writes a warning back to elasticsearch
         ICMPTesting(THRESHOLD, jsc);
 
     }
@@ -55,33 +38,25 @@ public class IDSJava {
 
         long lastCheck = getLastCheck();
         Object[] icmpAndTime = getOnlyICMPEvents(esRDD1, lastCheck);
-
-
         List<Map<String, Object>> ICMPevents = (List<Map<String, Object>>) icmpAndTime[0];
-        System.out.println("------------------------------------------------------------");
-        System.out.println("SIZE: " + ICMPevents.size());
-        for (Map<String, Object> a : ICMPevents){
-            System.out.println(a.get("event").toString());
-        }
-        System.out.println("...........................................................");
+
         if (ICMPevents.size() > THRESHOLD){
-            writeEventToES(ICMPevents, jsc);
+            writeEventToES(ICMPevents, jsc, THRESHOLD);
         }
         updateLastCheck((Long)icmpAndTime[1]);
     }
 
     // writes an ICMP warning to ElasticSearch
-    private static void writeEventToES(List<Map<String, Object>> ICMPevents, JavaSparkContext jsc) {
-        System.out.println("ISITHERE?----------------------------------------------------------------------------");
+    private static void writeEventToES(List<Map<String, Object>> ICMPevents, JavaSparkContext jsc, int THRESHOLD) {
+        System.out.println("Writing events to Elasticsearch.");
         jsc.close();
         SparkConf confSave = new SparkConf().setAppName("myApp").setMaster("local");
         confSave.set("es.index.auto.create", "true");
         JavaSparkContext jscSave = new JavaSparkContext(confSave);
 
-        Map<String, ?> numbers = ImmutableMap.of("one", 1, "two", 2, "date", getCurrentDate(false));
-        Map<String, ?> airports = ImmutableMap.of("OTP", "Otopeni", "SFO", "San Fran", "date", getCurrentDate(false));
+        Map<String, ?> numbers = ImmutableMap.of("event","The number of ICMP events from the last check exceeded the specified threshold(" + THRESHOLD + ")", "ICMPcount", ICMPevents.size()+"", "date", getCurrentDate(false) + "");
 
-        JavaRDD<Map<String, ?>> javaRDD = jscSave.parallelize(ImmutableList.of(numbers, airports));
+        JavaRDD<Map<String, ?>> javaRDD = jscSave.parallelize(ImmutableList.of(numbers));
         JavaEsSpark.saveToEs(javaRDD, "threats/icmp");
     }
 
@@ -102,13 +77,18 @@ public class IDSJava {
 
     // called by ICMPTesting()
     public static Object[] getOnlyICMPEvents(JavaRDD<Map<String, Object>> esRDD1, long lastCheck){
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM d yyyy HH:mm:ss");
         List<Map<String, Object>> onlyICMPevents = new ArrayList<>();
         Long maxTime = 0L;
         try {
             for (Map<String, Object> a : esRDD1.collect()) {
-                String time = a.get("@timestamp").toString().split(" ")[3];
-                Date d = sdf.parse(time);
+                String[] date = a.get("@timestamp").toString().split(" ");
+                String time = date[3];
+                String month = date[1];
+                String day = date[2];
+                String year = date[5];
+                String wholeDate = String.format("%s %s %s %s",month, day, year, time );
+                Date d = sdf.parse(wholeDate);
                 if (d.getTime() > lastCheck){
                     onlyICMPevents.add(a);
                 }
